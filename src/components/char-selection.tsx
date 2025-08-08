5df053b7c2ef1af315a4b2de664b2ca86e9d3143
ddk <dattimio95@gmail.com>
2025-08-08 17:47:10 +0200

    chore: rework Select

--- src/components/char-selection.tsx
+++ src/components/char-selection.tsx
@@ -24,37 +24,39 @@ function CharacterSelectGroup() {
 }
 
 interface CharacterSelectProps {
-  characters: string[];
   updateCharacters: (index: number, value: string) => void;
 }
 
 export default function CharacterSelect(props: CharacterSelectProps) {
   const { updateCharacters } = props;
-  const characterPlaceholder = ["DPS", "Slot 2", "Slot 3"];
+  const placeholder = ["DPS", "Slot 2", "Slot 3"];
 
   return (
     <div className="grid grid-cols-3">
       <Select onValueChange={(value) => updateCharacters(0, value)}>
         <SelectTrigger className="w-[170px] bg-gray-200 text-black border-gray-700 border-1 rounded-none focus:ring-0 focus:outline-none focus:ring-transparent">
-          <SelectValue placeholder={"DPS"} />
+          <SelectValue placeholder={placeholder[0]} />
         </SelectTrigger>
         <SelectContent>
           <CharacterSelectGroup />
         </SelectContent>
       </Select>
-      {characterPlaceholder.splice(1).map((placeholder, index) => (
-        <Select
-          key={index}
-          onValueChange={(value) => updateCharacters(index, value)}
-        >
-          <SelectTrigger className="w-[170px] rounded-none focus:ring-0 focus:outline-none focus:ring-transparent">
-            <SelectValue placeholder={placeholder} />
-          </SelectTrigger>
-          <SelectContent>
-            <CharacterSelectGroup />
-          </SelectContent>
-        </Select>
-      ))}
+      <Select onValueChange={(value) => updateCharacters(1, value)}>
+        <SelectTrigger className="w-[170px] rounded-none focus:ring-0 focus:outline-none focus:ring-transparent">
+          <SelectValue placeholder={placeholder[1]} />
+        </SelectTrigger>
+        <SelectContent>
+          <CharacterSelectGroup />
+        </SelectContent>
+      </Select>
+      <Select onValueChange={(value) => updateCharacters(2, value)}>
+        <SelectTrigger className="w-[170px] rounded-none focus:ring-0 focus:outline-none focus:ring-transparent">
+          <SelectValue placeholder={placeholder[2]} />
+        </SelectTrigger>
+        <SelectContent>
+          <CharacterSelectGroup />
+        </SelectContent>
+      </Select>
     </div>
   );
 }
