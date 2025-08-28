1726796f542bac2ce2d0c757fd96304e572ea250
ddk <dattimio95@gmail.com>
2025-08-28 13:33:08 +0200

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import CalculatorContent from "@/components/calc-content";
+import Footer from "@/components/footer";
 
 export default function Page() {
-  return <CalculatorContent />;
+  return (
+    <>
+      <CalculatorContent />
+      <Footer />
+    </>
+  );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bg-card text-card-foreground h-72 w-full">
+      <div className="max-w-6xl mx-auto py-6 px-12">
+        footer
+      </div>
+    </div>
+  );
+}
